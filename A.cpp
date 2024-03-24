9a74d2f4ab81572d6cdd721a0ad0007fb3420e78
mahmud34033 <abdullahalmahmud033@gmail.com>
2024-03-25 05:40:29 +0600

    demo commit

---
+++ A.cpp
@@ -0,0 +1,17 @@
+// print But not print
+
+#include <bits/stdc++.h>
+#include <math.h>
+#include <algorithm>
+#include <string>
+#include <numeric>
+#define ll long long
+#define nl endl
+using namespace std;
+
+int main(){
+    int n = 2;
+    for(int i = 1; i <= 4; i++) n = pow(n,2);
+    cout << n << nl;
+    return 0;
+}
